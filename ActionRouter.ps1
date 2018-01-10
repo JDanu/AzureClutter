25f45a101f4a7dece4ea4188c6a36189d0161341
JDanu <35098252+JDanu@users.noreply.github.com>
2018-01-10 02:31:36 -0800

    Adding Scale set and availability set routing

--- ActionRouter.ps1
+++ ActionRouter.ps1
@@ -10,12 +10,20 @@ Param
 )
 
 
-Write-Host $SelectedResource.ResourceName
+Write-Host $"Selected resource: $SelectedResource.ResourceName"
 
 switch ($SelectedResource.ResourceType)
 {
     "Microsoft.Compute/virtualMachines" {
-        Write-Host "Resource: StandAlone VM."      
+        Write-Host "Resource Type: StandAlone VM."      
         .\Resource\VM-Standalone.ps1 -ResourceGroupName $ResourceGroupName -ResourceName $SelectedResource.ResourceName 
+    }
+    "Microsoft.Compute/virtualMachineScaleSets"{
+        Write-Host "Resource Type: Virtual Machine Scale Set."
+        .\Resource\VM-ScaleSet.ps1 -ResourceGroupName $ResourceGroupName -ResourceName $SelectedResource.ResourceName
+    }
+    "Microsoft.Compute/availabilitySets" {
+        Write-Host "Resource Type: Availability Set"
+        .\Resource\VM-AvailabilitySet.ps1 -ResourceGroupName $ResourceGroupName -ResourceName $SelectedResource.ResourceName
     }
 }
